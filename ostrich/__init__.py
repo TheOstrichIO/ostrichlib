a52b4e3abc6368a74e8155dc13d196d8573df81d
Itamar Ostricher <itamarost@gmail.com>
2016-03-29 13:39:57 +0300

    dev5 bump

--- ostrich/__init__.py
+++ ostrich/__init__.py
@@ -4,5 +4,5 @@ OstrichLib - Pythoning with an Ostrich
 """
 
 __author__ = 'Itamar Ostricher'
-__version__ = '0.0.0.dev4'
+__version__ = '0.0.0.dev5'
 __oneliner__ = 'Ostrich Lib - A Bunch of Simple Useful Stuff'
